5dcfefb2b6ef93a5a682886619896c7cc5a58954
Pete Schuster <pas5027@gmail.com>
2013-08-07 15:33:48 -0400

    prop is quicker than attr

--- js/jquery.get-input-type.js
+++ js/jquery.get-input-type.js
@@ -4,7 +4,7 @@
     $.fn.getInputType = function () {
 
         return this[0].tagName.toString().toLowerCase() === "input" ?
-              $(this[0]).attr("type").toLowerCase() :
+              $(this[0]).prop("type").toLowerCase() :
               this[0].tagName.toLowerCase();
 
     }; // getInputType
